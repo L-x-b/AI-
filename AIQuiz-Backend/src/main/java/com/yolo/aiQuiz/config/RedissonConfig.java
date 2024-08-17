f66c93502882a161229f62f1c6b05088d18f39d0
yolo <919616056@qq.com>
2024-08-17 16:42:01 +0800

    修改 配置文件

--- AIQuiz-Backend/src/main/java/com/yolo/aiQuiz/config/RedissonConfig.java
+++ AIQuiz-Backend/src/main/java/com/yolo/aiQuiz/config/RedissonConfig.java
@@ -8,9 +8,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Data
-@Configuration
-@ConfigurationProperties(prefix = "spring.redis")
+//@Data
+//@Configuration
+//@ConfigurationProperties(prefix = "spring.redis")
 public class RedissonConfig {
 
     private String host;
@@ -21,14 +21,14 @@ public class RedissonConfig {
 
     private String password;
 
-    @Bean
-    public RedissonClient redissonClient() {
-        Config config = new Config();
-        config.useSingleServer()
-                .setAddress("redis://" + host + ":" + port)
-                .setDatabase(database)
-                .setPassword(password);
-        return Redisson.create(config);
-    }
+//    @Bean
+//    public RedissonClient redissonClient() {
+//        Config config = new Config();
+//        config.useSingleServer()
+//                .setAddress("redis://" + host + ":" + port)
+//                .setDatabase(database)
+//                .setPassword(password);
+//        return Redisson.create(config);
+//    }
 
 }
